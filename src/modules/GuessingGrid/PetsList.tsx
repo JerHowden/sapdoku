f8eb8a494546288ee9db97ad74339d56ed70a031
Jeremiah Howden <jeremiahhowden@gmail.com>
2025-02-10 16:48:45 -0600

    memoize select pet button

--- src/modules/GuessingGrid/PetsList.tsx
+++ src/modules/GuessingGrid/PetsList.tsx
@@ -3,11 +3,40 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { Pet } from '@/db';
 import Image from 'next/image';
+import { memo } from 'react';
 
 type PetsListProps = {
   pets: Pet[];
   choosePet: (pet: Pet) => void;
 };
+
+type PetSelectButtonProps = {
+  pet: Pet;
+  choosePet: (pet: Pet) => void;
+};
+
+const PetSelectButton = memo(function PetSelectButton({ pet, choosePet }: PetSelectButtonProps) {
+  return (
+    <ToggleGroupItem
+      key={pet.name}
+      value={pet.name}
+      size="auto"
+      onClick={() => choosePet(pet)}
+      className="py-3 px-6 w-full justify-start data-[state=on]:text-primary"
+    >
+      <div className="flex flex-row items-center gap-6 text-sm sm:text-base md:text-lg">
+        <Image
+          src={pet.imageSrc || 'https://superautopets.wiki.gg/images/0/0b/Rock.png'}
+          alt={pet.name}
+          width={80}
+          height={80}
+          className="w-[32px] sm:w-[48px] md:w-[80px]"
+        />
+        {pet.name}
+      </div>
+    </ToggleGroupItem>
+  );
+});
 
 export function PetsList({ pets, choosePet }: PetsListProps) {
   return (
@@ -18,24 +47,11 @@ export function PetsList({ pets, choosePet }: PetsListProps) {
           className="flex-col gap-2 p-6"
         >
           {pets.map((pet) => (
-            <ToggleGroupItem
+            <PetSelectButton
               key={pet.name}
-              value={pet.name}
-              size="auto"
-              onClick={() => choosePet(pet)}
-              className="py-3 px-6 w-full justify-start data-[state=on]:text-primary"
-            >
-              <div className="flex flex-row items-center gap-6 text-sm sm:text-base md:text-lg">
-                <Image
-                  src={pet.imageSrc || 'https://superautopets.wiki.gg/images/0/0b/Rock.png'}
-                  alt={pet.name}
-                  width={80}
-                  height={80}
-                  className="w-[32px] sm:w-[48px] md:w-[80px]"
-                />
-                {pet.name}
-              </div>
-            </ToggleGroupItem>
+              pet={pet}
+              choosePet={choosePet}
+            />
           ))}
         </ToggleGroup>
       </ScrollArea>
